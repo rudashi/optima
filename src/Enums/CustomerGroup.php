e64e7386dd331e40050d49aed9731ba4de6fa3d4
Rudashi <rudashi@gmail.com>
2023-06-15 08:24:54 +0200

    refactor: Added new CustomerGroup cases

--- src/Enums/CustomerGroup.php
+++ src/Enums/CustomerGroup.php
@@ -6,6 +6,15 @@ namespace Rudashi\Optima\Enums;
 
 enum CustomerGroup: string
 {
+    case TRADING_COMPANY = 'FIRMA HANDLOWA';
+    case HEALTHCARE = 'OPIEKA ZDROWOTNA';
+    case FOREIGN_COMPANY = 'ZAGRANICZNA FIRMA';
+    case PUBLISHING_HOUSE = 'WYDAWNICTWO';
+    case INDIVIDUAL = 'OSOBA FIZYCZNA';
+    case INSTITUTION = 'INSTYTUCJA';
+    case SERVICE_COMPANY = 'FIRMA USŁUGOWA';
+    case UNIVERSITY = 'WYŻSZA UCZELNIA';
+    case TRANSPORT = 'TRANSPORTOWA';
     case SUPPLIER = 'DOSTAWCA';
     case SUBCONTRACTOR = 'PODWYKONAWCA';
 }
