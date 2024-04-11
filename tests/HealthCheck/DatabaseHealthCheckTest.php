77bf1715213c62ad003e0b6a423ed277e087c463
rudashi <rudashi@gmail.com>
2024-04-11 12:36:39 +0200

    tests: Update tests

--- tests/HealthCheck/DatabaseHealthCheckTest.php
+++ tests/HealthCheck/DatabaseHealthCheckTest.php
@@ -26,7 +26,7 @@ it('shows problem when database driver is incorrect', function () {
 });
 
 it('shows problem when driver is not installed on server', function () {
-    config(['database.connections.optima.driver' => 'mariadb']);
+    config(['database.connections.optima.driver' => '__mariadb']);
     $service = new DatabaseHealthCheckService(app('db'));
 
     expect($service->status())
@@ -39,7 +39,7 @@ it('shows problem when driver is not installed on server', function () {
         ->toHaveKey('status', $service::PROBLEM)
         ->toHaveKey('message', 'Could not connect to db')
         ->toHaveKey('context.connection', 'optima')
-        ->toHaveKey('context.exception.error', 'Unsupported driver [mariadb].');
+        ->toHaveKey('context.exception.error', 'Unsupported driver [__mariadb].');
 });
 
 it('can connect to optima database', function () {
