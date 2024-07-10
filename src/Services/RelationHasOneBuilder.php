39964da5870ee247b45497321aa46803da81bf55
Borys Żmuda <rudashi@gmail.com>
2024-07-10 08:06:19 +0200

    feat: Added new QueryBuilder Relation - HasOne

---
+++ src/Services/RelationHasOneBuilder.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rudashi\Optima\Services;
+
+class RelationHasOneBuilder extends RelationBuilder
+{
+    protected function buildDictionary(object|array $models, string $key): array
+    {
+        if (is_array($models)) {
+            $dictionary = [];
+
+            foreach ($models as $relation) {
+                $attribute = is_iterable($relation) ? $relation->get(0)?->{$key} : $relation->{$key};
+
+                $dictionary[$attribute] = $relation;
+            }
+
+            return $dictionary;
+        }
+
+        return parent::buildDictionary($models, $key);
+    }
+}
